5d8d5da8eaafd1fb79cdb22eaa37f63316f40bb2
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-17 17:57:25 +0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {HeaderModule} from "./features/header/components/header.module";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
