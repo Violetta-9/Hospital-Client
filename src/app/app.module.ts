4be540cc2af250e16e41370524b7c9aa54b7029e
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-20 13:48:04 +0300

    Update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import {DialogModule} from '@angular/cdk/dialog'
 import {HeaderModule} from "./features/header/components/header.module";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { LoginModalComponent } from './features/user/modals/login-modal/login-modal.component';
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatInputModule} from "@angular/material/input";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {UserModal} from "./features/user/user.modal";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {registerLocaleData} from "@angular/common";
+import localeEn from '@angular/common/locales/en';
+
+registerLocaleData(localeEn, 'en');
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginModalComponent,
+
+
 
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     HeaderModule,
-    FontAwesomeModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatInputModule,
-    DialogModule
+    UserModal,
+    NgbModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      useDefaultLang: true,
+      defaultLanguage: "en"
+    })
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule  { }
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return new TranslateHttpLoader(http, './assets/locale/', '.json');
+}
+
