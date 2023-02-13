ac718858d5eea55ff0c828648f45954fa70e7913
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-13 16:59:41 +0300

    update registration modal

--- src/app/features/user/modals/registration-modal/registration-modal.component.ts
+++ src/app/features/user/modals/registration-modal/registration-modal.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {EntityDetailsBaseComponent} from "../../../../core/components/abstraction/entity-detail-base.component";
 import {MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../../core/services/swagger-gen/authorization";
+import {passwordValidator} from "../../../../shared/validators/passwordValidator";
+import {emailValidators} from "../../../../shared/validators/emailValidator";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-registration-modal',
@@ -14,8 +18,9 @@ export class RegistrationModalComponent extends EntityDetailsBaseComponent imple
 
   constructor(
     public dialogRef: MatDialogRef<RegistrationModalComponent>,
-
+    public authorizationApi: UserService,
     public dialog: MatDialog,
+    private toastr: ToastrService
 
   ) {
     super()
@@ -28,14 +33,15 @@ export class RegistrationModalComponent extends EntityDetailsBaseComponent imple
 
   private _createForm() {
     this.detailsForm = new FormGroup({
-      login: new FormControl('',[Validators.maxLength(256), Validators.required,]),
       firstName: new FormControl('',[Validators.maxLength(256), Validators.required]),
       lastName: new FormControl('',[Validators.maxLength(256), Validators.required]),
-      email: new FormControl('', [Validators.maxLength(256), Validators.required]),
+      middleName: new FormControl('',[Validators.maxLength(256), Validators.required]),
+      email: new FormControl('', [Validators.maxLength(256), Validators.required,emailValidators()]),
       password: new FormControl('',[ Validators.required]),
       passwordConfirm: new FormControl('', [ Validators.required]),
       phoneNumber: new FormControl('',[Validators.maxLength(15), Validators.required]),
-    })
+      birthDate: new FormControl('',[ Validators.required])},
+    passwordValidator)
   }
 
 
@@ -43,6 +49,19 @@ export class RegistrationModalComponent extends EntityDetailsBaseComponent imple
 
 
   saveInternal() {
+    console.log(this.detailsForm.getRawValue());
+    this.authorizationApi.registerUser(this.detailsForm.getRawValue()).subscribe(
+      result =>{
+        if(result){
+          this.dialogRef.close();
+          this.showSuccess();
+        }
+      }
+    )
 
   }
+  showSuccess() {
+    this.toastr.success('You have registered.Check your email', 'Success!');
+  }
+
 }
