751beea3678c2398cf4eeeb7bf41a59507e36879
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-13 17:00:11 +0300

    update login modal

--- src/app/features/user/modals/login-modal/login-modal.component.ts
+++ src/app/features/user/modals/login-modal/login-modal.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {EntityDetailsBaseComponent} from "../../../../core/components/abstraction/entity-detail-base.component";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatDialog, MatDialogRef} from "@angular/material/dialog";
-
+import {UserService} from "../../../../core/services/swagger-gen/authorization";
+import jwt_decode from "jwt-decode";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-login-modal',
@@ -10,10 +12,12 @@ import {MatDialog, MatDialogRef} from "@angular/material/dialog";
   styleUrls: ['./login-modal.component.css']
 })
 export class LoginModalComponent extends EntityDetailsBaseComponent implements OnInit {
-  hide=true
-  constructor( public dialogRef: MatDialogRef<LoginModalComponent>,
-               public dialog: MatDialog,
+  hide = true
 
+  constructor(public dialogRef: MatDialogRef<LoginModalComponent>,
+              public dialog: MatDialog,
+              public authorizationApi: UserService,
+              private toastr: ToastrService
   ) {
     super();
     this.createForm();
@@ -24,9 +28,9 @@ export class LoginModalComponent extends EntityDetailsBaseComponent implements O
   }
 
   private createForm() {
-    this.detailsForm=new FormGroup({
-      login: new FormControl('',Validators.required),
-      password:new FormControl('',Validators.required),
+    this.detailsForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
     })
   }
 
@@ -42,9 +46,64 @@ export class LoginModalComponent extends EntityDetailsBaseComponent implements O
   //   });
   // }
   saveInternal() {
+    this.authorizationApi.loginForm(
+      this.detailsForm.get('email').value,
+       this.detailsForm.get('password').value
+    ).subscribe(x=>{
+      console.log(x);
+      if(x){
+        console.log("mau");
+        const f=jwt_decode(x)
+        console.log(f)
+        // @ts-ignore
+        if(f.role.includes("Receptionist")) {
+          console.log("mauR");
+          this.dialogRef.close();
+          localStorage.setItem('token', x);
+          // @ts-ignore
+          localStorage.setItem('role', f.role);
+          this.showSuccessForReceptionist();
+        }
+        // @ts-ignore
+        else if(f.role.includes("Doctor")) {
+          console.log("mauD");
+          this.dialogRef.close();
+          localStorage.setItem('token', x);
+          // @ts-ignore
+          localStorage.setItem('role', f.role);
+          this.showSuccessForDoctor();
+        }
+        // @ts-ignore
+       else if(f.role.includes("Patient")) {
+          console.log("mauP");
+          this.dialogRef.close();
+          localStorage.setItem('token', x);
+          // @ts-ignore
+          localStorage.setItem('role', f.role);
+          this.showSuccessForPatient();
+        }
 
-
-    };
-
-
+      else {
+          this.dialogRef.close();
+          localStorage.setItem('token', x);
+          // @ts-ignore
+          localStorage.setItem('role', f.role);
+          this.showSuccessForUser();
+        }
+        }
+    })
+  }
+  showSuccessForDoctor() {
+    this.toastr.success('You have successfully logged in to your account as Doctor', 'Success!');
+  }
+  showSuccessForReceptionist() {
+    this.toastr.success('You have successfully logged in to your account as Receptionist', 'Success!');
+  }
+  showSuccessForPatient() {
+    this.toastr.success('You have successfully logged in to your account as Patient', 'Success!');
+  }
+  showSuccessForUser() {
+    this.toastr.success('You have successfully logged in to your account as User', 'Success!');
+  }
 }
+
