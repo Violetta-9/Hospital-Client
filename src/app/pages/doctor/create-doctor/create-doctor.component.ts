9dc71e5db4a6c275485634349c0b416549264a62
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-28 18:38:49 +0300

    create doctor

--- src/app/pages/doctor/create-doctor/create-doctor.component.ts
+++ src/app/pages/doctor/create-doctor/create-doctor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DoctorService, ReceptionistService} from "../../../core/services/swagger-gen/profile";
+import dateFormat, { masks } from "dateformat";
 @Component({
   selector: 'app-create-doctor',
   templateUrl: './create-doctor.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateDoctorComponent implements OnInit {
 public userRole='doctor'
-  constructor() { }
+  constructor(public doctorService:DoctorService) { }
 
   ngOnInit(): void {
   }
 
+  createDoctor(doctor: any) {
+
+    this.doctorService.assignRoleToDoctorForm(doctor.firstName,doctor.lastName,doctor.middleName,doctor.email,
+      doctor.phoneNumber,dateFormat(doctor.birthDate, "isoDateTime"),doctor.specialization,doctor.office,doctor.status,
+      doctor.file).subscribe(x=>console.log(x))
+  }
 }
