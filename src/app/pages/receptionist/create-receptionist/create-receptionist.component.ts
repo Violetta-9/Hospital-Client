92d7d36fbdbb657a8bb712896eaf4153c3786086
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-28 18:39:07 +0300

    create receptionist

--- src/app/pages/receptionist/create-receptionist/create-receptionist.component.ts
+++ src/app/pages/receptionist/create-receptionist/create-receptionist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ReceptionistService} from "../../../core/services/swagger-gen/profile";
+import dateFormat, { masks } from "dateformat";
 @Component({
   selector: 'app-create-receptionist',
   templateUrl: './create-receptionist.component.html',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateReceptionistComponent implements OnInit {
 public userRole='receptionist'
-  constructor() { }
+  public s=0;
+  constructor(public receptionist:ReceptionistService) { }
 
   ngOnInit(): void {
   }
 
+  createReceptionist(repo: any) {
+  console.log("i am here recep")
+    this.s=2;
+    this.receptionist.assignReceptionistRoleForm(repo.firstName,repo.lastName,repo.middleName,
+      repo.email,repo.phoneNumber,dateFormat(repo.birthDate, "isoDateTime"),
+     repo.office,repo.file).subscribe(x=>console.log(x))
+  }
 }
