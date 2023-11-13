774c0cc0ad93e7ad625f5598ec60d8bc075ebc24
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-11-13 16:31:53 +0300

    codeGen for office

---
+++ client-generation/src/app/core/services/swagger-gen/office/model/models.ts
@@ -0,0 +1,6 @@
+export * from './allOfficesDto';
+export * from './body';
+export * from './body1';
+export * from './officeDto';
+export * from './response';
+export * from './updateOfficeStatusDTO';
