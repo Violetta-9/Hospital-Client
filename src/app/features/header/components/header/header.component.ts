ea0e105190fc06d0937151514ac5aae721c685f1
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-12-26 16:04:07 +0300

    refactor

--- src/app/features/header/components/header/header.component.ts
+++ src/app/features/header/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {LoginModalComponent} from "../../../user/modals/login-modal/login-modal.component";
 import {RegistrationModalComponent} from "../../../user/modals/registration-modal/registration-modal.component";
+import { Router } from '@angular/router';
+import { AllOfficesDto, OfficeService } from '../../../../core/services/swagger-gen/office';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +11,12 @@ import {RegistrationModalComponent} from "../../../user/modals/registration-moda
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  public isHidden: boolean=false;
-  constructor(public dialog: MatDialog) { }
+  public isHidden: boolean = false;
+  offices: AllOfficesDto[];
+  constructor(public dialog: MatDialog,private readonly router:Router,private readonly officeService: OfficeService) { }
 
   ngOnInit(): void {
+    this.officeService.getAllOffices().subscribe(x=>this.offices = x)
   }
   openLoginDialog(){
     console.log("op Work")
@@ -32,7 +36,28 @@ export class HeaderComponent implements OnInit {
     return localStorage.getItem('token')!==null;
   }
 
-  logOut() {
+  async logOut() {
     localStorage.clear();
+   await this.router.navigate(['/']);
+  }
+
+  goTo(id: number) {
+    this.router.navigate(["office"], { queryParams: { id: id } });
+  }
+
+  get getToManagePage():string {
+    let roles = localStorage.getItem('role');
+    let rolesArray = roles.split(',');
+    if(rolesArray.includes('Patient')){
+      return '/management/patient'
+    } else if(rolesArray.includes('Doctor')){
+      return '/management/doctor'
+    }else if(rolesArray.includes('Receptionist')){
+      return '/management/receptionist'
+    }
+    else{
+      return ''
+    }
+
   }
 }
